3e50ded9a7f0c056eb1c0dba8f515b3a01ed485f
Daniel Qiu <qiudaniel03@gmail.com>
2023-01-15 15:04:47 -0500

    add tailwind css to be uniform across the app

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Paper, Checkbox } from '@mui/material'
+import { Checkbox } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import ItemEditPopup from './ItemEditPopup';
@@ -26,14 +26,14 @@ const Item = ({url, itemId, itemTitle, itemDesc, isCompleted}) => {
       })
     }
     const handleEditClick = (e) => {
-      setPopupVisible(true);
+      setPopupVisible(!popupVisible);
       console.log(popupVisible)
     }
     const [popupVisible, setPopupVisible] = useState(false);
   return (
 
     <div class="container p-2 pr-10 ">
-    <div class="h-50 w-full rounded-lg bg-white m-4 p-3 outline outline-red-100">
+    <div class="h-50 w-full rounded-lg bg-white m-4 p-3 outline outline-gray-300">
         <div class="flex items-center justify-between border-b">
             <div class="p-3 text-gray-700 text-lg font-bold">{itemTitle}</div>
             <div class="p-3 flex">
@@ -50,8 +50,8 @@ const Item = ({url, itemId, itemTitle, itemDesc, isCompleted}) => {
             {itemDesc}
         </div>
         <button onClick={handleEditClick} class="text-slate-800 hover:text-blue-600 text-sm bg-white hover:bg-slate-100 border border-slate-200 rounded-l-lg rounded-r-lg font-medium px-4 py-2 inline-flex space-x-1 items-center">
-                    <EditIcon class="w-4 h-4"/>
-                    <span>Edit</span>
+            <EditIcon class="w-4 h-4"/>
+            <span>Edit</span>
         </button>
         <ItemEditPopup visible={popupVisible} setVisible={setPopupVisible} id={itemId}itemTitle={itemTitle} itemDesc={itemDesc} url={url}></ItemEditPopup>
     </div>
